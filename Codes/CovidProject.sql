fd4d0d5aa36a7d7c6f13189f0b11a2f33a90199e
Suraj Jadhav <110662518+Suraj200105@users.noreply.github.com>
2022-09-21 17:04:19 +0530

    Add files via upload

---
+++ Codes/CovidProject.sql
@@ -0,0 +1,24 @@
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From Project..covidDeathData
+where continent is not null 
+order by 1,2
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From Project..covidDeathData
+Where continent is null 
+and location not in ('World', 'European Union', 'International','Upper middle income','High income','Lower middle income','Low income')
+Group by location
+order by TotalDeathCount desc
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From Project..covidDeathData
+where location not in ('World', 'European Union', 'International','Upper middle income','High income','Lower middle income','Low income')
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+Select continent, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From Project..covidDeathData
+Where continent is not null 
+and continent not in ('World', 'European Union', 'International','Upper middle income','High income','Lower middle income','Low income')
+Group by continent, Population, date
+order by PercentPopulationInfected desc
