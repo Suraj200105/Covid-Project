d898184348ac3d654572de5821b0bff29cb92bcc
Suraj Jadhav <110662518+Suraj200105@users.noreply.github.com>
2022-09-21 17:06:40 +0530

    Update CovidProject.sql

--- Codes/CovidProject.sql
+++ Codes/CovidProject.sql
@@ -1,7 +1,11 @@
+--STATS
+
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From Project..covidDeathData
 where continent is not null 
 order by 1,2
+
+--TOTAL DEATH COUNT BY CONTINENT
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From Project..covidDeathData
@@ -10,11 +14,15 @@ and location not in ('World', 'European Union', 'International','Upper middle in
 Group by location
 order by TotalDeathCount desc
 
+--PERCENTAGE OF POPULATION INFECTED BY COUNTRY
+
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From Project..covidDeathData
 where location not in ('World', 'European Union', 'International','Upper middle income','High income','Lower middle income','Low income')
 Group by Location, Population
 order by PercentPopulationInfected desc
+
+--PERCENTAGE OF POPULATION INFECTED 
 
 Select continent, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From Project..covidDeathData
